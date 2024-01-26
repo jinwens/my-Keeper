43b5f113baf30728ac766df5c9fa0538c4034bd0
Jackson Sun <jackson@Jacksons-MacBook-Pro-14.local>
2024-01-25 21:30:56 -0600

    Version 1 s Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //1. Create a new React app.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/App";
+import App from "./components/App.jsx";
 
 ReactDOM.render(
     <App type="text/babel" />,
